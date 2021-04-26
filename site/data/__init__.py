32b51f24234183cbb66f2a7bd6c42903628e5d9e
PhantomOfficiaI <74786947+PhantomOfficiaI@users.noreply.github.com>
2021-04-26 21:03:18 +0300

    Delete site directory

--- site/data/__init__.py
+++
@@ -1,5 +0,0 @@
-from .models import users
-from .models import notes
-from .models.notes import Notes
-from .models.users import User
-__all__ = [users, notes, Notes, User]
--- site/forms/add_note.py
+++
@@ -1,8 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import SubmitField, TextAreaField
-from wtforms.validators import DataRequired
-
-
-class AddNoteForm(FlaskForm):
-    text = TextAreaField('Write here something', validators=[DataRequired()])
-    submit = SubmitField('Ok')
